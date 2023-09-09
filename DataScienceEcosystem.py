f1f8bcb20acdb4cd71ba527360ee9b228eb5ee80
Phaneendhra07 <144537172+Phaneendhra07@users.noreply.github.com>
2023-09-09 18:20:59 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem<h1>
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - List popular programming languages for Data Science.
+# - Introduce arithmetic expressions and operations in Python.
+# - Explore basic data manipulation techniques in pandas.
+# - Demonstrate data visualization using matplotlib and seaborn.
+# - Provide examples of machine learning model training and evaluation.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# <ol>
+# <li>Python</li>
+# <li>R</li>
+# <li>Julia</li>
+# </ol>
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     <ol>
+# <li>numpy</li>
+# <li>Tensorflow</li>
+# <li>matplotlic</li>
+# <li>scikit-learn</li>
+# </ol>
+
+# 
+# <tr>
+# <th>Data Science Tools</th></tr>
+# <tr>Rstudio</tr>
+# <tr>apache spark</tr>
+# <tr>apache hadoop</tr>
+# <tr>rapid miner</tr>
+# 
+
+# | Data Science Tools      |
+# |------------------------|
+# | Jupyter Notebook       |
+# | RStudio                |
+# | VSCode with Python     |
+# 
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+# # Addition
+# result_add = 5 + 3
+# 
+# # Subtraction
+# result_sub = 10 - 4
+# 
+# # Multiplication
+# result_mul = 6 * 7
+# 
+# # Division (Floating-Point)
+# result_div_float = 15 / 4
+# 
+# # Division (Integer)
+# result_div_int = 15 // 4
+# 
+# # Exponentiation
+# result_exp = 2 ** 3
+# 
+# # Modulus (Remainder)
+# result_mod = 17 % 5
+# 
+# 
+# 
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integersresult=(3*4)+5
+result=(3*4)+5
+print(result)
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# ## Author
+# -author
+# 
+
+# In[ ]:
+
+
+
+
